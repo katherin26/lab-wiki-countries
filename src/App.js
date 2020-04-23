b9dd8fade3d506cc6704a21ff681b4ba67a2b6f8
Tzikas <niko.tzikas@gmail.com>
2020-04-23 11:20:26 -0400

    wiki

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import './App.css';
+import { Switch, Route } from 'react-router-dom';
+import Navbar from './NavBar'
+import Home from './Home'
+import About from './About'
+import Contact from './Contact'
+import CountryDetail from './CountryDetail'
+
+class App extends Component {
+
+  state = { }
+
+  componentDidMount(){ //window.onload happens once when the components first mounts 
+    console.log(this)  //Get your data from an API or where you'll get data from your server DB. 
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Welcome to Routing</h1>
+        <Navbar /> {/*  This will always show because it not in the switch statement  */}
+        <Switch>
+          <Route exact path='/' component={(props) =>   <Home {...props} /> }></Route>
+          <Route exact path='/about' component={(props) =>    <About {...props} /> }></Route>
+          <Route exact path='/contact' component={(props) =>  <Contact {...props} /> }></Route>
+          <Route path='/country/:id' component={(props) => <CountryDetail {...props} />}></Route>
+          {/* //https://open.spotify.com/playlist/37i9dQZF1E39v21gLOnzCG */}
+        </Switch>
+      </div>
+    );
+  }
+}
+
+export default App;
